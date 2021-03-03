cf34ad8bb2ed4253b8506365637c6c73799ad07c
mucheniski <mucheniski@gmail.com>
2021-03-03 16:00:51 -0300

    Project created

---
+++ src/test/java/com/example/convertbitmaptobase64/ConvertBitmapToBase64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertbitmaptobase64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertBitmapToBase64ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
