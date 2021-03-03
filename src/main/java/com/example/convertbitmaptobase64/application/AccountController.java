af5cbf720d0cc5853c971fc861424986d96cc590
mucheniski <mucheniski@gmail.com>
2021-03-03 17:02:52 -0300

    find all and find by id done

--- src/main/java/com/example/convertbitmaptobase64/application/AccountController.java
+++ src/main/java/com/example/convertbitmaptobase64/application/AccountController.java
@@ -1,9 +1,30 @@
 package com.example.convertbitmaptobase64.application;
 
+import com.example.convertbitmaptobase64.domain.domain.Account;
+import com.example.convertbitmaptobase64.domain.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
+
+    @Autowired
+    private AccountService service;
+
+    @GetMapping
+    public List<Account> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Account findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
 }
--- src/main/java/com/example/convertbitmaptobase64/domain/service/AccountService.java
+++ src/main/java/com/example/convertbitmaptobase64/domain/service/AccountService.java
@@ -1,7 +1,25 @@
 package com.example.convertbitmaptobase64.domain.service;
 
+import com.example.convertbitmaptobase64.domain.domain.Account;
+import com.example.convertbitmaptobase64.domain.exception.EntityNotFoundException;
+import com.example.convertbitmaptobase64.domain.port.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository repository;
+
+    public List<Account> findAll() {
+        return repository.findAll();
+    }
+
+    public Account findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Not found with id " + id));
+    }
+
 }
